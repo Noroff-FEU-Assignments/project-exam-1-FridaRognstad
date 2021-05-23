464f7c19c7cc9c7fad09296d4dce77b5eef2923e
Frida Rognstad <71266828+FridaRognstad@users.noreply.github.com>
2021-05-23 20:03:06 +0200

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -1,11 +1,13 @@
 const apiUrl =
   "https://fridarognstad.one/projectexam/wordpress/wp-json/wp/v2/posts";
 const indexApiResults = document.querySelector(".index-api-results");
+const latestPostsSection = document.querySelector(".latest-posts");
 
+//fetching blogposts to carousel
 async function fetchIndexApi() {
   try {
     const data = await fetch(
-      apiUrl + `?page=${carouselIndex}&per_page=${perPageIndex}`
+      apiUrl + `?page=${carouselIndex}&per_page=${perPageIndex}&_embed`
     );
     const json = await data.json();
 
@@ -13,15 +15,19 @@ async function fetchIndexApi() {
 
     createIndexHtml(json);
   } catch (error) {
-    console.log(error);
+    console.log("Could not call the API");
+    latestPostsSection.innerHTML = message(
+      "Sorry, something went wrong loading the posts :("
+    );
   }
 }
 
+//creating html to carousel
 function createIndexHtml(json) {
   for (let i = 0; i < json.length; i++) {
     indexApiResults.innerHTML += `<div class="blog-results">
                                     <a href="blog-post.html?id=${json[i].id}">
-                                    <img class="blog-image" src="${json[i].featured_image_src.full}">
+                                    <img class="blog-image" alt="${json[i]._embedded["wp:featuredmedia"][0].alt_text}" src=${json[i]._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url}">
                                     <p class="date">${json[i].published_on}</p>
                                     <h3>${json[i].title.rendered}</h3>
                                     </a>
@@ -34,6 +40,7 @@ const previousButton = document.querySelector(".left");
 const nextButton = document.querySelector(".right");
 let carouselIndex = 1;
 
+//arrows clickable and make the carousell endless
 function next() {
   carouselIndex++;
   if (carouselIndex === 4) {
@@ -52,6 +59,7 @@ function previous() {
 previousButton.addEventListener("click", previous);
 nextButton.addEventListener("click", next);
 
+//adjust visable posts inside carousel considering screen-size
 let perPageIndex = 1;
 
 function checkMediaQuery() {
@@ -64,10 +72,19 @@ function checkMediaQuery() {
   }
 }
 
-// Initial check
 checkMediaQuery();
 
-// Add a listener for when the window resizes
 window.addEventListener(`resize`, checkMediaQuery);
 
+// reload indexpage when resized to update carousel
+let resizeReload;
+
+window.addEventListener("resize", function () {
+  clearTimeout(resizeReload);
+  resizeReload = setTimeout(function () {
+    window.location.reload();
+  }, 1500);
+});
+
 fetchIndexApi(apiUrl);
+
